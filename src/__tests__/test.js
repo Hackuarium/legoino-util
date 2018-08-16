e61a2c1d918ff7c0d97a868606992c753c628d5d
Luc Patiny <luc@patiny.com>
2018-08-16 08:00:22 +0200

    base project

---
+++ src/__tests__/test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const legoinoUtil = require('..');
+
+describe('legoino-util test', () => {
+    it('Something to test', () => {
+        expect(legoinoUtil()).toBe(true);
+    });
+});
---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const defaultOptions = {};
+
+function legoinoUtil(options) {
+    options = Object.assign({}, defaultOptions, options);
+    return true;
+}
+
+module.exports = legoinoUtil;
